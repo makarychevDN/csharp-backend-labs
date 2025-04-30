4ed4f5b1371163551717e1b1a6e45db538891e9d
makarychevDN <39443709+makarychevDN@users.noreply.github.com>
2025-04-30 19:33:02 +0500

    lab 1.1 has been added

---
+++ lab1.1/ConsoleApp1/Program.cs
@@ -0,0 +1,16 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        int age = 25;
+        Console.Write("введите имя: ");
+        string name = Console.ReadLine();
+        GreetUser(name, age);
+    }
+
+    private static void GreetUser(string name, int age)
+    {
+        Console.WriteLine($"Привет, я {name}! Сегодня {DateTime.Now:dd.MM.yyyy}");
+        Console.WriteLine($"Мне {age} лет");
+    }
+}
