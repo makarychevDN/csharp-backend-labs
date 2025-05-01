bb1e8892cf9bafc487c2e0df095e5ba78ea4756c
makarychevDN <39443709+makarychevDN@users.noreply.github.com>
2025-05-01 20:28:34 +0500

    now requests return Results instead of objects

--- lab1.4/lab1.4/Program.cs
+++ lab1.4/lab1.4/Program.cs
@@ -7,17 +7,24 @@ var books = new List<Book>();
 
 app.MapGet("/books", () =>
 {
-    return books;
+    return Results.Ok(books);
 });
 
 app.MapGet("/books/{id}", (int id) =>
 {
-    return books[id];
+    if (id >= books.Count)
+        return Results.BadRequest($"the book with id {id} is non found");
+
+    return Results.Ok(books[id]);
 });
 
 app.MapPost("/books", (Book book) =>
 {
+    if (book == null)
+        return Results.BadRequest("can't add null as a book");
+
     books.Add(book);
+    return Results.Created($"/books/{books.IndexOf(book)}", book);
 });
 
 app.Run();
